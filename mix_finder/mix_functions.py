821fef108be14dac6691e868d99d0e59840da5ad
Daniel Maloney <84576669+Desperate-Dan@users.noreply.github.com>
2022-04-25 16:07:10 +0100

    Update mix_functions.py
    Updating the ambiguous base finder

--- mix_finder/mix_functions.py
+++ mix_finder/mix_functions.py
@@ -9,15 +9,16 @@ import re
 
 from Bio import SeqIO
 
-# first function to parse the input fasta files and look for Ns.
-def fasta_reader(input_fasta):
+# first function to parse the input fasta files and look for non-AGCTs.
+def ambig_base_finder(input_fasta,min=1,max=3):
+  seq_dict = {}
   for record in SeqIO.parse(input_fasta, "fasta"):        
-    N_bases = re.finditer("[AGCT][^ACGT]{1,3}[AGCT]", str(record.seq))
+    N_bases = re.finditer("[AGCT][^AGCT]{" + str(min) + "," + str(max) + "}[AGCT]", str(record.seq))
     coord_list = []
     for mo in N_bases:
         N_coord = mo.start()
         coord_list.append(N_coord)
         print(coord_list)
-    seq_dict[record.id] = coord_list  
+    seq_dict[record.id] = coord_list
+  return(seq_dict)
 
-print(seq_dict)
